9a75f82afec22fad3ce7705c6e4286f747573319
Yulia <yulia.kovalova.741@gmail.com>
2021-03-16 05:01:30 +0200

    added adaptive mobile header, burger menu, slider, and started to work with first section 'About us'

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+// burger menu mobile version
+let mobileMenu = document.querySelector(".menu-mobile-burger");
+let mainMenu = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active-menu")
+    if (mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu")
+    } else {
+        mainMenu.classList.remove("active-menu")
+    }
+})
+
 // Add slider slick
 $(".slider-block").slick({
-    autoplay:true,
+    // autoplay:true,
     autoplaySpeed:3000,
     dots:true,
+    infinite: true,
+    slidesToShow: 1,
+    adaptiveHeight: true,
 })
